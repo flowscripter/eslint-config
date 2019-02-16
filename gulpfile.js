d94ebd756f01b0d51b17a05d7a71fea4e2e2b62b
vectronic <hello.vectronic@gmail.com>
2019-02-16 21:42:30 +0000

    fix: sonar reporting fix: npm badge

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,13 @@ function sonar(callback) {
             options: {
                 'sonar.sources': 'src',
                 'sonar.tests': 'test',
-                'sonar.projectKey': 'flowscripter_eslint-config'
+                'sonar.projectKey': 'flowscripter_eslint-config',
+                'sonar.links.homepage': 'https://www.npmjs.com/package/@flowscripter/eslint-config',
+                'sonar.links.ci': 'https://travis-ci.com/flowscripter/eslint-config',
+                'sonar.links.scm': 'https://github.com/flowscripter/eslint-config',
+                'sonar.links.issue': 'https://github.com/flowscripter/eslint-config/issues',
+                'sonar.javascript.lcov.reportPaths': 'reports/lcov.info',
+                'sonar.eslint.reportPaths': 'reports/eslint.json'
             }
         },
         callback
