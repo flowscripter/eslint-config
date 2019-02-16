c623cf1a1facc4351d675271fc58bb571dac2cd6
vectronic <hello.vectronic@gmail.com>
2019-02-16 19:24:31 +0000

    fix: again trying to get it working

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ const sonarqubeScanner = require('sonarqube-scanner');
 function sonar(callback) {
     sonarqubeScanner(
         {
+            serverUrl: 'https://sonarcloud.io',
+            token: process.env.SONAR_TOKEN,
             options: {
                 'sonar.sources': 'src',
                 'sonar.tests': 'test',
                 'sonar.projectKey': 'flowscripter_eslint-config',
                 'sonar.organization': 'flowscripter',
-                'sonar.login': process.env.SONAR_TOKEN
             }
         },
         callback
